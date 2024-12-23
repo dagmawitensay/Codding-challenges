e237f7ae37ce20b9d01e3dee6557e722bd5a9f0e
dagmawitensay <dagitensay@gmail.com>
2024-12-23 18:49:40 +0300

    set up SQLAlchemy engine and base model for database connection

--- URL_shortner/initialjlfize_db.py
+++
@@ -1,6 +0,0 @@
-from app.database import Base, engine
-from app.models.url_mapping import UrlMapping
-
-print("Creating database table")
-Base.metadata.create_all(bind=engine)
-print("Tables created successfully.")
